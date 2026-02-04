cef628cc52a9aaa7e2148fb709295630d3b77251
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-02-04 01:59:52 -0700

    enabling compiler commands and flags in cmake build

---
+++ C++_Basics/build/CMakeFiles/streams.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/streams.dir/streams.cpp.o"
+  "CMakeFiles/streams.dir/streams.cpp.o.d"
+  "streams"
+  "streams.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/streams.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
