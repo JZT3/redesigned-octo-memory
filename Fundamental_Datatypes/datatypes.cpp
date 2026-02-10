fc75769b08e053d673f28ba9dac9869914deaa24
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-02-10 12:51:43 -0800

    added type info

---
+++ Fundamental_Datatypes/datatypes.cpp
@@ -0,0 +1,57 @@
+/*
+Memory is organized into sequential units called Memory Addresses
+they allow us to find and access contents of memory at a specific location
+
+Memory addresses hold 1 byte of data (8 SEQUENTIAL bits)
+
+
+
+Data types tell the compiler how to interpret the contents of memory.
+
+
+
+Fundamnetal (Integral) Types
+
+  bool
+
+  char
+  wchar_t
+  char8_t        (C++20)
+  char16_t       (C++11)
+  char32_t       (C++11)
+
+  short int
+  int
+  long int
+  long long int  (C++11)
+
+
+Other Fundamnetal Types
+  float
+  double
+  long double
+
+  std::nullptr_t (C++11)
+
+  void
+
+
+Fixed Width Integer Types
+   int8_t
+  int16_t
+  int32_t
+  int64_t
+
+   uint8_t
+  uint16_t
+  uint32_t
+  uint64_t
+
+   intmax_t
+  uintmax_t
+
+
+
+
+*/
+
