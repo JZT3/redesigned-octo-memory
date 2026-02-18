a732b0b3a0bf6320f8f1358c89d0580168e14d29
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-02-18 15:45:59 -0800

    going over basic sizes of fundamental data types

---
+++ Fundamental_Datatypes/sizes.cpp
@@ -0,0 +1,32 @@
+/*
+ A single object may use 1,2,4,8 or more contiguous memory addresses.
+ The amount of memory an object uses is determined by its data type.
+
+ we typically access memory through variable names and not memory addresses
+
+
+ The size of an object puts a limit on how many unique values it can store.
+    objects that store more bytes can store larger num of unique values
+ 
+
+Computers have finite memory.
+everytime you define an object it is used for that lifetime
+
+
+An object must take minimum 1 byte such that it can have a distinct memory address 
+  a byte is at least 8 bits
+  a memory byte is addressable (we can access every byte in memory independently)
+  Floating point support is IEEE-754 compliant
+  32 or 64 bit architecture
+
+
+bool      1 byte
+char
+wchar_t
+char8_t
+char16_t
+char32_t
+
+
+
+*/
