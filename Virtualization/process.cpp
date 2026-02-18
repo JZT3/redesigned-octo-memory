9f018fc351a95881c664f58cb003ab27f7d72c75
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-02-18 15:47:22 -0800

    notes from chapter 1 of OS 3 easy pieces of virtualization

---
+++ Virtualization/process.cpp
@@ -0,0 +1,11 @@
+/*
+ A process (informally is a running program)
+    A program is just a bunch of instructions and static data on disk
+    The operating system takes these bytes and gets them running.
+
+
+ Although there are few physical CPUs available, the OS can provide the illusion of many
+
+ When you want to run more than on programs the OS can virtualize the CPU
+
+ */
