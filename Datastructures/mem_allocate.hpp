983f3f6339dd659e10c0914841fcfe5f8db7491d
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-02-18 16:55:33 -0800

    completed basic version of memory allocator

--- Datastructures/mem_allocate.hpp
+++ Datastructures/mem_allocate.hpp
@@ -2,7 +2,11 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstdint>
 #include <mutex>
+#include <sys/mman.h>>
+#include <unistd.h>
+
 
 namespace simple_alloc {
 
@@ -33,7 +37,7 @@ namespace simple_alloc {
 
 
   // COMPILE-TIME CHECKS
-  static_assert(sizeof(BlockHeader)  % ALIGNMENT == 0, "Header must be aligned");
+  //static_assert(sizeof(BlockHeader)  % ALIGNMENT == 0, "Header must be aligned");
   static_assert(sizeof(BlockFooter) <= ALIGNMENT,      "Footer fits in padding");
 
   constexpr std::size_t HEADER_SIZE = sizeof(BlockHeader);
@@ -161,7 +165,7 @@ namespace simple_alloc {
                      MAP_PRIVATE | MAP_PRIVATE,
                      -1,0);
 
-    if (mem == MAP_PRIVATE)
+    if (mem == MAP_FAILED)
     {
       return nullptr;
     } 
@@ -224,6 +228,123 @@ namespace simple_alloc {
 
       if (mem == MAP_FAILED) return nullptr;
 
-
+      auto* b = reinterpret_cast<BlockHeader*>(mem);
+      *b = BlockHeader {
+              .size_alloc = total_needed,
+              .prev_free  = nullptr,
+              .next_free  = nullptr
+      };
+      set_alloc(b, true);
+      set_mmap(b, true);
+      write_footer(b);
+      return ptr_add(b, HEADER_SIZE);
     }
+
+
+    // Search Free List (First Fit strategy)
+    BlockHeader* curr = free_list_head;
+    while (curr) 
+    {
+      if (get_size(curr) >= total_needed) break;
+      curr =  curr->next_free;
+    }
+
+
+    // Extend Heap
+    if (!curr) 
+    {
+      if (!request_chunk(total_needed)) return nullptr;
+      curr = free_list_head; // We know the new chunck is at the head
+    }
+
+    remove_free(curr);
+
+    std::size_t current_size = get_size(curr);
+    std::size_t remaining    = current_size - total_needed;
+
+    if (remaining >= MIN_BLOCK_SIZE)
+    {
+      auto* split = ptr_add<BlockHeader*>(curr, total_needed);
+
+      *split = BlockHeader {
+                  .size_alloc = remaining,
+                  .prev_free  = nullptr,
+                  .next_free  = nullptr
+                };
+
+      write_footer(split);
+      insert_free(split);
+
+      curr->size_alloc = total_needed;
+    }
+
+    set_alloc(curr, true);
+    write_footer(curr);
+
+    return ptr_add(curr, HEADER_SIZE);
   }
+
+
+  void free(void* ptr) 
+  {
+    if (!ptr) return;
+
+    std::scoped_lock lock(global_lock);
+
+    auto* b = ptr_add<BlockHeader*>(ptr, -static_cast<std::ptrdiff_t>(HEADER_SIZE));
+
+    if (is_mmap(b))
+    {
+      munmap(b, get_size(b));
+      return;
+    }
+
+    set_alloc(b, false);
+    write_footer(b);
+
+    insert_free(b);
+  }
+
+  void* calloc(std::size_t n, std::size_t size)
+  {
+    if (n != 0 && size > SIZE_MAX /n ) return nullptr;
+
+    std::size_t total = n * size;
+    void* p = malloc(total);
+
+    if (p)
+    {
+      std::memset(p,0,total);
+    }
+    return p;
+  }
+
+  void* realloc(void* ptr, std::size_t size)
+  {
+    if (!ptr) return malloc(size);
+    if (size == 0) 
+    {
+      free(ptr);
+      return nullptr;
+    }
+
+    auto* b = ptr_add<BlockHeader*>(ptr, -static_cast<std::ptrdiff_t>(HEADER_SIZE));
+    std::size_t old_size = get_size(b) - HEADER_SIZE - FOOTER_SIZE;
+
+    if (old_size >= size) return ptr;
+
+    void* new_ptr = malloc(size);
+    if (new_ptr)
+    {
+      std::memcpy(new_ptr, ptr, old_size);
+      free(ptr);
+    }
+    return new_ptr;
+  } 
+
+}
+
+
+
+
+
